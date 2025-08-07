06f51bb884709e52669b3d89f53e82e38f67fb00
Ricardo Calix <rcalix1@gmail.com>
2025-08-07 17:55:58 -0500

    Update index.js

--- Deployment/index.js
+++ Deployment/index.js
@@ -36,204 +36,41 @@ async function runExample1() {
 
 
 
-
-async function runExample141() {
-
+async function runExample2() {
     
- 
   const x = new Float32Array(4);
-    
-  x[0] = parseFloat(document.getElementById('box0c1').value) || 0;
-  x[1] = parseFloat(document.getElementById('box1c1').value) || 0;
-  x[2] = parseFloat(document.getElementById('box2c1').value) || 0;
-  x[3] = parseFloat(document.getElementById('box3c1').value) || 0;
+  x[0] = parseFloat(document.getElementById('box0c2').value) || 0;
+  x[1] = parseFloat(document.getElementById('box1c2').value) || 0;
+  x[2] = parseFloat(document.getElementById('box2c2').value) || 0;
+  x[3] = parseFloat(document.getElementById('box3c2').value) || 0;
 
- 
   const tensorX = new ort.Tensor('float32', x, [1, 4]);
 
   try {
-  
-    const session = await ort.InferenceSession.create(
-      "./resNet_Inverse_realData_new.onnx?v=" + Date.now() // cache-bust
-    );
-
- 
+    const session = await ort.InferenceSession.create("./resNet_Inverse_realData_new.onnx?v=" + Date.now());
     const results = await session.run({ input1: tensorX });
-    const output = results.output1.data; // Float32Array
+    const output = results.output1.data; // Float32Array length 7
 
-  
-    //console.log("Output vector:", output);
-    //alert("First value: " + output[0].toFixed(2));
-
+    // render here (output is in scope)
+    const predictions = document.getElementById('predictions2');
+    predictions.innerHTML = `
+      <hr>Got an output Tensor:<br/>
+      <table>
+        <tr><td>i_h2i_rate</td>          <td id="c2td0">${output[0].toFixed(2)}</td></tr>
+        <tr><td>i_h2_temp</td>           <td id="c2td1">${output[1].toFixed(2)}</td></tr>
+        <tr><td>i_ngi_rate</td>          <td id="c2td2">${output[2].toFixed(2)}</td></tr>
+        <tr><td>i_pci_rate</td>          <td id="c2td3">${output[3].toFixed(2)}</td></tr>
+        <tr><td>i_o2_volfract</td>       <td id="c2td4">${output[4].toFixed(2)}</td></tr>
+        <tr><td>i_hbtemp</td>            <td id="c2td5">${output[5].toFixed(2)}</td></tr>
+        <tr><td>i_wind_rt</td>           <td id="c2td6">${output[6].toFixed(2)}</td></tr>
+      </table>`;
   } catch (e) {
     console.error("ONNX runtime error:", e);
     alert("Error: " + e.message);
   }
-
- // PREDS DIV 
-  let predictions = document.getElementById('predictions1');
-  
-
-  predictions.innerHTML = `<hr> Got an output Tensor with values being: <br/> 
- <table>
- 
-  <tr>
-  <td> i_h2i_rate</td>
-  <td id="c1td0"> ${ output[0].toFixed(2)   } </td>
-  </tr>
-  
-  <tr>
-  <td> i_h2_temp </td>
-  <td id="c1td1"> ${output[1].toFixed(2)} </td>
-  </tr> 
-
-  <tr>
-  <td> i_ngi_rate </td>
-  <td id="c1td2"> ${output[2].toFixed(2)} </td>
-  </tr> 
-
-    <tr>
-  <td> i_h2i_rate</td>
-  <td id="c1td3"> ${ output[3].toFixed(2)   } </td>
-  </tr>
-  
-  <tr>
-  <td> i_h2_temp </td>
-  <td id="c1td4"> ${output[4].toFixed(2)} </td>
-  </tr> 
-
-  <tr>
-  <td> i_ngi_rate </td>
-  <td id="c1td5"> ${output[5].toFixed(2)} </td>
-  </tr> 
-
-  <tr>
-  <td> i_ngi_rate </td>
-  <td id="c1td6"> ${output[6].toFixed(2)} </td>
-  </tr> 
-
- 
- </table>   `;
- 
-
-
-
-    
 }
 
 
-
-
-
-async function runExample7() {
-    
-
-  var x = new Float32Array(1, 4);
-
-  var x = [];
-  x[0] = document.getElementById('box0c1').value;
-  x[1] = document.getElementById('box1c1').value;
-  x[2] = document.getElementById('box2c1').value;
-  x[3] = document.getElementById('box3c1').value;
-
- 
-  let tensorX = new onnx.Tensor(x, 'float32', [1, 4]);
-  
-   
-  let session1 = new onnx.InferenceSession();
-  await session1.loadModel("./resNet_Inverse_realData.onnx");
-    
-  let outputMap1 = await session1.run([tensorX]);
-  let outputData1 = outputMap1.get('output1');
-
-    
- 
- 
-
-  // PREDS DIV 
-  let predictions = document.getElementById('predictions1');
-
-
- alert("hello");
-
-
-  predictions.innerHTML = `<hr> Got an output Tensor  with values being: <br/> 
-here   `;
- 
-
-
-}
-
-
-async function runExample2() {
-    
-
-
-  var x = new Float32Array(1, 6);
-
-  var x = [];
-  x[0] = document.getElementById('box0c2').value;
-  x[1] = document.getElementById('box1c2').value;
-  x[2] = document.getElementById('box2c2').value;
-  x[3] = document.getElementById('box3c2').value;
-  x[4] = document.getElementById('box4c2').value;
-  x[5] = document.getElementById('box5c2').value;
- 
-
-  let tensorX = new onnx.Tensor(x, 'float32', [1, 6]);
-  
-   
-  let session1 = new onnx.InferenceSession();
-  await session1.loadModel("./F1plusF2NNs_raceway_coal_burn_perce.onnx");
-  let outputMap1 = await session1.run([tensorX]);
-  let outputData1 = outputMap1.get('output1');
-
-
-  let session2 = new onnx.InferenceSession();
-  await session2.loadModel("./F1plusF2NNs_raceway_flame_temp_k.onnx");
-  let outputMap2 = await session2.run([tensorX]);
-  let outputData2 = outputMap2.get('output1');
-
-    
-  let session3 = new onnx.InferenceSession();
-  await session3.loadModel("./F1plusF2NNs_raceway_volume_m.onnx");
-  let outputMap3 = await session3.run([tensorX]);
-  let outputData3 = outputMap3.get('output1');
-  
- 
-
-  // PREDS DIV 
-  let predictions = document.getElementById('predictions2');
-  
- 
-
-
-  predictions.innerHTML = `<hr> Got an output Tensor with values being: <br/> 
- <table>
- 
-  <tr>
-  <td> o_raceway_coal_burn_perce</td>
-  <td id="c2td0"> ${outputData1.data[0].toFixed(2)} </td>
-  </tr>
-  
-  <tr>
-  <td> o_raceway_flame_temp_k </td>
-  <td id="c2td1"> ${outputData2.data[0].toFixed(2)} </td>
-  </tr> 
-
-  <tr>
-  <td> o_raceway_volume_m </td>
-  <td id="c2td2"> ${outputData3.data[0].toFixed(2)} </td>
-  </tr> 
-  
-
- 
- </table>   `;
- 
-
-
-
-}
 
 
 async function runDiff() {
